602edab8d97eabfe600a76e125b08af51d53b327
Edward Hill <edwardtayloehill@gmail.com>
2017-01-10 16:59:53 -0500

    cart line items almost complete and ready for review

---
+++ db/models/cart_line_item.js
@@ -0,0 +1,13 @@
+'use strict'
+const Sequelize = require('sequelize');
+const db = require('APP/db');
+
+const Cart_Line_Item = db.define('cart_line_item', {
+  quantity:{
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: { notEmpty: true }
+  }
+})
+
+module.exports = Cart_Line_Item;
--- db/models/index.js
+++ db/models/index.js
@@ -8,11 +8,20 @@ const User = require('APP/db/models/user');
 const Product = require('APP/db/models/product');
 const Address = require('APP/db/models/address');
 const Product_Review = require('APP/db/models/product_review')
+const Cart_Line_Item = require('APP/db/models/cart_line_item')
+
 
 Address.belongsTo(User)
+
 Product_Review.belongsTo(User)
 Product_Review.belongsTo(Product)
+
+Cart_Line_Item.belongsTo(User)
+Cart_Line_Item.belongsTo(Product)
+
+
 // Order.belongsTo(User)
 // Order.belongsTo(Address, {as: 'shipping_address'})
 // Order.belongsTo(Address, {as: 'billing_address'})
-module.exports = { User, Product, Address, Product_Review };
+
+module.exports = { User, Product, Address, Product_Review, Cart_Line_Item };
