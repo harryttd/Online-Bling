bafd7c56cd4f082b5c6e8a3ceeb774be914f46a9
Edward Hill <edwardtayloehill@gmail.com>
2017-01-10 18:10:11 -0500

    removed exrtra file

--- db/models/cart-line-items.js
+++
@@ -1,9 +0,0 @@
-const Sequelize = require('sequelize');
-const db = require('APP/db');
-
-const Cart_Line_Item = db.define('cart_line_item', {
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: { notEmpty: true }
-  },
