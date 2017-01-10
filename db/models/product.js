3f9833746615bfb0b7ced0b28fb83a678c62c05e
Aryeh Harris <harryttd@gmail.com>
2017-01-09 21:28:16 -0500

    closes #4 4-product-model. Add default image in public folder.

--- db/models/product.js
+++ db/models/product.js
@@ -2,7 +2,29 @@ const Sequelize = require('sequelize');
 const db = require('APP/db');
 
 const Product = db.define('product', {
-  name: Sequelize.STRING
+  name: {
+    type: Sequelize.STRING,
+  },
+  sku: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    unique: true
+  },
+  description: {
+    type: Sequelize.JSON
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 1 // Create a function that increases quantity.
+  },
+  image: {
+    type: Sequelize.STRING,
+    defaultValue: 'APP/public/default-photo.png'
+  }
 });
 
 module.exports = Product;
--- tests/product.test.js
+++ tests/product.test.js
@@ -1,10 +1,43 @@
 const expect = require('chai').expect;
-// const Product = require('products');
 const Product = require('APP/db/models/product');
 const db = require('APP/db');
 
 describe('Product Model', function() {
-  it('should exist', function() {
-    expect(Product).toExist();
+
+  before(function () {
+    return db.didSync;
   });
+
+  let product;
+  beforeEach(function(){
+    product = Product.build({
+      name: 'Ring',
+      sku: 'SKURING',
+      description: { main: 'Really Pretty', size: 'baby' },
+      price: 1000,
+      quantity: 10
+    });
+  });
+
+  //  We empty the tables after each spec
+  afterEach(function () {
+    return Product.truncate({ cascade: true });
+  });
+
+  describe('model attributes', function() {
+
+    it('should exist', function() {
+      expect(Product).to.exist;
+    });
+
+    it('should have: name, sku, description, price, quantity, image', function() {
+      expect(product.name).to.equal('Ring');
+      expect(product.sku).to.equal('SKURING');
+      expect(product.description).to.deep.equal({ main: 'Really Pretty', size: 'baby' });
+      expect(product.price).to.equal(1000);
+      expect(product.quantity).to.equal(10);
+      expect(product.image).to.equal('APP/public/default-photo.png');
+    });
+  });
+
 });
