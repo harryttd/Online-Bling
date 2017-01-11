7726a081b53f864f7017fc4d27e058a1c7e77145
Tae Hoon Lee <thlee1122@hotmail.com>
2017-01-11 18:26:13 -0500

    Finished Categories routing and testing

--- server/routes/categories.js
+++ server/routes/categories.js
@@ -1,3 +1,62 @@
 const db = require('APP/db');
 const express = require('express');
 const router = module.exports = express.Router()
+const Category = require('APP/db/models/category');
+
+router.get('/', (req, res, next) => 
+	Category.findAll()
+	.then(categories => res.status(200).json(categories))
+	.catch(next))
+
+
+router.get('/:id', (req, res, next) =>
+	Category.findById(req.params.id)
+	.then(category => res.json(category))
+	.catch(next))
+
+
+router.post('/', (req, res, next) => 
+	Category.create(req.body)
+	.then(order => res.status(201).json(order))
+	.catch(next))
+
+
+router.put('/:id', (req, res, next) => 
+	Category.update(req.body,
+		{
+			where: {
+				id: req.params.id
+			}
+		})
+		.then(count => 
+			res.status(201).json(updated)
+		)
+		.catch(next))
+
+
+router.put('/:id', (req, res, next) =>
+    Category.update( req.body, 
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then((count, updated)  => {
+      res.status(201).json(updated[0])
+    })
+    .catch(next))
+
+
+router.delete('/:id', (req, res, next) =>
+    Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then( deleted => res.sendStatus(204).end())
+    .catch(next));
+
+
+
+
+module.exports = router;
