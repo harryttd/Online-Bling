29b9c75d4ca620239b226fc22d6490972eb339d0
Aryeh Harris <harryttd@gmail.com>
2017-01-10 19:00:44 -0500

    add route files and folders

--- server/api.js
+++ server/api.js
@@ -1,17 +1,21 @@
 'use strict'
 
-const db = require('APP/db')
+const db = require('APP/db');
 const api = module.exports = require('express').Router()
 
 api
   .get('/heartbeat', (req, res) => res.send({ok: true,}))
-  .use('/auth', require('./auth'))
-  .use('/users', require('./users'))
-
+  .use('/auth', require('./routes/auth'))
+  .use('/users', require('./routes/users'))
+  .use('/address', require('./routes/address'))
+  .use('/cart', require('./routes/cart-line-item'))
+  .use('/category', require('./routes/categories'))
+  .use('/order', require('./routes/order'))
+  .use('/product', require('./routes/product'));
 // Send along any errors
 api.use((err, req, res, next) => {
-  res.status(500).send(err)
-})
+  res.status(500).send(err);
+});
 
 // No routes matched? 404.
-api.use((req, res) => res.status(404).end())
+api.use((req, res) => res.status(404).end());
