001677e563711e70e19482e4afdb08941e3e8d2c
Aryeh Harris <harryttd@gmail.com>
2017-01-11 15:08:17 -0500

    Complete initial product route and initial testing

--- server/routes/product.js
+++ server/routes/product.js
@@ -1,3 +1,36 @@
-const db = require('APP/db');
+const { Product, Category } = require('APP/db/models');
 const express = require('express');
-const router = module.exports = express.Router()
+
+module.exports = express.Router()
+  .get('/', (req, res, next) =>
+    Product.findAll()
+    .then(products => res.json(products))
+    .catch(next))
+  .get('/:productId', (req, res, next) =>
+    Product.findById(req.params.productId)
+    .then(product => res.json(product))
+    .catch(next))
+  .get('/category/:categoryName', (req, res, next) =>
+    Category.findOne({
+      where: { name: req.params.categoryName }
+    })
+    .then(category => category.getProducts())
+    .then(products => res.json(products))
+    .catch(next))
+  .post('/', (req, res, next) =>
+    // Set up auth stuff
+    Product.create(req.body)
+    .then(newProduct => res.status(201).json(newProduct))
+    .catch(next))
+  .put('/:productId', (req, res, next) =>
+    // Set up auth stuff
+    Product.findById(req.params.productId)
+    .then(targetProduct => targetProduct.update(req.body))
+    .then(updatedProduct => res.status(202).send(updatedProduct))
+    .catch(next))
+  .delete('/:productId', (req, res, next) =>
+    // Set up auth stuff
+    Product.findById(req.params.productId)
+    .then(targetProduct => targetProduct.destroy())
+    .then(() => res.sendStatus(204))
+    .catch(next));
