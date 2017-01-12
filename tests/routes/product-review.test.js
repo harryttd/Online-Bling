98f780786127084710ea1e8f7f20bed727761ec7
Tae Hoon Lee <thlee1122@hotmail.com>
2017-01-12 00:10:21 -0500

    Finished Product-Review Routing and test

---
+++ tests/routes/product-review.test.js
@@ -0,0 +1,86 @@
+const request = require('supertest-as-promised');
+const {expect} = require('chai');
+const db = require('APP/db');
+const productReview = require('APP/db/models/product_review');
+const app = require('APP/server/start');
+
+describe.only('product review route', () => {
+	before('wait for the db', () => db.didSync);
+
+	let productReviewData = {
+		body: 'Good product',
+		star: '4'
+	}
+
+	before('Build Product instance', () => 
+		productReview.create(productReviewData)
+	);
+
+	// after(() => 
+	// 	productReview.truncate({cascade: true})
+	// );
+
+	describe('routings', () => {
+		it('Get / should return all product reviews', () => 
+			request(app)
+			.get(`/api/productReview`)
+			.expect(200)
+			.then(res => {
+				expect(res.body[0].body).to.equal('Good product');
+				// expect(res.body[0].star).to.equal('4');
+			})
+
+		)
+
+		it('GET /:id should return one product review with corresponding id', () => 
+ 			request(app)
+ 				.get(`/api/productReview/1`)
+ 				.expect(200)
+ 				.then(res => {
+ 					expect(res.body.body).to.equal('Good product')
+ 					// expect(res.body.star).to.equal('4');
+ 				})
+ 		)
+
+		it('POST / creates a product review', () => 
+ 			request(app)
+ 				.post(`/api/productReview/`)
+ 				.send({
+ 					body: 'Awesome', star: '3'
+ 				})
+ 				.expect(201)
+ 				.then(res => {
+ 					expect(res.body.body).to.equal('Awesome')
+ 				})
+ 		)
+
+ 		it('PUT /:id update a product review with corresponding id', () => 
+ 			request(app)
+ 				.put(`/api/productReview/1`)
+ 				.send({
+ 					body: 'Great', star: 3
+ 				})
+ 				.then(updateProductReview => {
+ 					return productReview.findById(1)
+ 					.then (productReview => {
+ 						expect(productReview.body).to.equal('Great');
+ 						// expect(productReview.star).to.equal('3');
+ 					})
+ 				})
+ 		)
+
+ 		it('DELETE /:id removes a product review with corresponding id', () => 
+ 			request(app)
+ 				.delete(`/api/productReview/1`)
+ 				.expect(204)
+ 		)
+
+	})
+})
+
+
+
+
+
+
+
