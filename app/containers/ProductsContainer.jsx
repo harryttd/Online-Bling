adbf560d660c142d058f6db809736c60a6dbadc4
Aryeh Harris <harryttd@gmail.com>
2017-01-13 11:59:15 -0500

    Add more component and container files. Add reducer and action test folders.

---
+++ app/containers/ProductsContainer.jsx
@@ -0,0 +1,12 @@
+import { connect } from 'react-redux';
+import { Products } from '../components/Products';
+
+const mapState = state => {
+
+};
+
+const mapDispatch = dispatch => {
+
+};
+
+export default connect(mapState, mapDispatch)(Products);
