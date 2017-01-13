66f50fc7b27e84c981fb3c9c9cb7cde5c56eabd7
Aryeh Harris <harryttd@gmail.com>
2017-01-12 23:02:03 -0500

    Change Homepage component definition to just export default

--- app/components/Homepage.jsx
+++ app/components/Homepage.jsx
@@ -1,28 +1,22 @@
 import React, { Component } from 'react';
-import { Router, Route, Link } from 'react-router'
-import Navbar from './Navbar'
+import { Link } from 'react-router';
+import Navbar from './Navbar';
 
-export default class Homepage extends Component {
-
-	render () {
-		console.log('test time')
-		return (
-
-			<div className="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <h1>Online Bling</h1>
-        <div className="navbar-header">
-          <a className="navbar-brand clearfix" >
-					</a>
-        </div>
-        <div className="collapse navbar-collapse">
-          <Navbar/>
-        </div>
-        <div className="container">
-          <img src="https://d1y7kerpdff7pl.cloudfront.net/Drake%20Necklace.jpg" />
-          <button type="button" className="main-btn">SHOP NOW</button>
-        </div>
+export default () => {
+	console.log('test time');
+	return (
+		<div className="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <h1>Online Bling</h1>
+      <div className="navbar-header">
+        <a className="navbar-brand clearfix" />
       </div>
-
-			)
-	}
-}
+      <div className="collapse navbar-collapse">
+        <Navbar />
+      </div>
+      <div className="container">
+        <img src="https://d1y7kerpdff7pl.cloudfront.net/Drake%20Necklace.jpg" />
+        <button type="button" className="main-btn">SHOP NOW</button>
+      </div>
+    </div>
+	);
+};
