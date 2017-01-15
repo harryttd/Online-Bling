f89d222efb3d8f8e1a110a4ea1410d29524ba7e2
Aryeh Harris <harryttd@gmail.com>
2017-01-15 15:51:00 -0500

    Got logout to function on navbar.

--- app/containers/RoutesContainer.jsx
+++ app/containers/RoutesContainer.jsx
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import Routes from '../routes';
 import { getProducts, getProductById } from '../thunks-dispatchers/products-dispatchers';
+// import { retrieveLoggedInUser } from '../action-creators/auth';
 
 const mapState = null;
 
