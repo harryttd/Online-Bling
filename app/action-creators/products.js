ab660cfc9f2da828aadc66ae7dad0d6d8380f363
Aryeh Harris <harryttd@gmail.com>
2017-01-16 13:15:40 -0500

    Moved Dispatchers back to action-creators. Moved tests out of duplicate folders.

--- app/action-creators/products.js
+++ app/action-creators/products.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { RECEIVE_PRODUCTS, RECEIVE_PRODUCT } from '../action-types';
 
 export const receiveProducts = products => ({
@@ -9,3 +10,13 @@ export const receiveProduct = product => ({
     type: RECEIVE_PRODUCT,
     product
 });
+
+export const getProducts = () => dispatch =>
+  axios.get('/api/product')
+  .then(response => dispatch(receiveProducts(response.data)))
+  .catch(error => console.error("Could Not Retrieve Products", error));
+
+export const getProductById = productId => dispatch =>
+  axios.get(`/api/product/${productId}`)
+  .then(response => dispatch(receiveProduct(response.data)))
+  .catch(error => console.error("Could Not Retrieve Product", error));
