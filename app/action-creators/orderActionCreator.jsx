3c71ff023438f17bd9ac39a28c9eee2a8c0593d5
Tae Hoon Lee <thlee1122@hotmail.com>
2017-01-17 17:56:54 -0500

    Order ActionCreator

---
+++ app/action-creators/orderActionCreator.jsx
@@ -0,0 +1,120 @@
+import axios from 'axios';
+import { browserHistory } from 'react-router'
+
+//***************** CONSTS
+
+//Users
+export const FETCH_ALL_ORDERS = 'FETCH_ALL_ORDERS';
+export const FETCH_SINGLE_ORDER = 'FETCH_SINGLE_ORDER';
+
+//Admins
+export const FETCH_ALL_ORDERS_ADMIN = 'FETCH_ALL_ORDERS_ADMIN';
+export const FETCH_SINGLE_ORDER_ADMIN = 'FETCH_SINGLE_ORDER_ADMIN';
+
+//****************** ACTIONS
+//Users
+export function getAllOrders(orders) {
+  return {
+    type: FETCH_ALL_ORDERS,
+    orders
+  }
+}
+
+export function getSingleOrder(currentOrder) {
+  return {
+    type: FETCH_SINGLE_ORDER,
+    currentOrder
+  }
+}
+
+//Admins
+export function getSingleOrderAdmin(currentOrder) {
+  return {
+    type: FETCH_SINGLE_ORDER_ADMIN,
+    currentOrder
+  }
+}
+
+export function getAllOrdersAdmin(orders) {
+  return {
+    type: FETCH_ALL_ORDERS_ADMIN,
+    orders
+  }
+}
+
+//******************* THUNKS
+
+//Users
+
+//Get all orders for the user
+export function fetchAllOrders() {
+  return function (dispatch, getState) {
+  	//get userId from auth
+    // const userId = getState().auth.id
+
+    axios.get(`/api/order/${userId}`)
+      .then(res => res.data)
+      .then(foundOrders => {
+        dispatch(getAllOrders(foundOrders))
+      })
+      .catch(console.error)
+    }
+}
+
+//Get One order for the user
+export function fetchSingleOrder(orderId) {
+  return function (dispatch, getState) {
+    // const userId = getState().auth.id
+
+    axios.get(`/api/order/${userId}/${orderId}`)
+      .then(res => res.data)
+      .then(foundOrder => {
+        dispatch(getSingleOrder(foundOrder))
+      })
+      .catch(console.error)
+    }
+} 
+
+//Admins
+
+//Get all orders for the admin
+export function fetchAllOrdersForAdmin() {
+  return function (dispatch, getState) {
+    axios.get('/api/orders/admin')
+      .then(res => res.data)
+      .then(foundOrders => {
+        dispatch(getAllOrdersAdmin(foundOrders))
+      })
+      .catch(console.error)
+    }
+}
+
+//Get one order for the admin
+export function fetchSingleOrderForAdmin(orderId) {
+  return function (dispatch, getState) {
+    axios.get(`/api/orders/admin/${orderId}`)
+      .then(res => res.data)
+      .then(foundOrder => {
+        dispatch(getSingleOrderAdmin(foundOrder))
+      })
+      .catch(console.error)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
