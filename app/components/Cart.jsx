befbeada78bf4f37483ba120a567cf73a2aec8bd
Aryeh Harris <harryttd@gmail.com>
2017-01-17 20:51:37 -0500

    If cart is empty, will not render the table of the cart. Will say cart is empty.

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -18,30 +18,34 @@ export default class Cart extends React.Component {
 
 				<section className="cart-list container-fluid">
 					<div className="row">
-						<div className="col-xs-12 col-md-8 col-md-offset-2">
-							<div>
-								<table className="cart-table table table-hover">
-									<thead>
-										<tr>
-											<th className="col-xs-2">Image</th>
-											<th className="col-xs-4">Name</th>
-											<th className="col-xs-2">Qty</th>
-											<th className="col-xs-2">Price</th>
-											<th className="col-xs-2">Action</th>
-										</tr>
-									</thead>
-									{
-										cartItems.map(item =>
-											<tbody key={ item.id }>
-												<CartItem item={item} removeItem={removeItem} updateQuantity={updateQuantity} />
-											</tbody>
-										)
-									}
-								</table>
-								<button onClick="" className="submit">check out
-								</button>
+						{
+							cartItems.length ?
+							<div className="col-xs-12 col-md-8 col-md-offset-2">
+								<div>
+									<table className="cart-table table table-hover">
+										<thead>
+											<tr>
+												<th className="col-xs-2">Image</th>
+												<th className="col-xs-4">Name</th>
+												<th className="col-xs-2">Qty</th>
+												<th className="col-xs-2">Price</th>
+												<th className="col-xs-2">Action</th>
+											</tr>
+										</thead>
+										{
+											cartItems.map(item =>
+												<tbody key={ item.id }>
+													<CartItem item={item} removeItem={removeItem} updateQuantity={updateQuantity} />
+												</tbody>
+											)
+										}
+									</table>
+									<button onClick="" className="submit">check out
+									</button>
+								</div>
 							</div>
-						</div>
+							: <h2>Your cart is currently empty</h2>
+						}
 					</div>
 				</section>
 			</div>
