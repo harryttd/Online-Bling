f8ccd2db1200af14cfcf5299703105a5c00387d8
Aryeh Harris <harryttd@gmail.com>
2017-01-28 20:34:12 -0500

    Fix indent issue in Cart component

--- app/components/Cart.jsx
+++ app/components/Cart.jsx
@@ -3,53 +3,53 @@ import { Link } from 'react-router';
 import CartItem from './CartItem';
 
 export default class Cart extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-	render() {
-		const { cartItems, removeItem, updateQuantity } = this.props;
-		return (
-			<div id="CartContainer" className="shopping-cart">
+  constructor(props) {
+    super(props);
+  }
+    render() {
+      const { cartItems, removeItem, updateQuantity } = this.props;
+      return (
+        <div id="CartContainer" className="shopping-cart">
 
-				<ol className="breadcrumb">
-					<li><Link to="/">Home</Link></li>
-					<li className="active">Cart</li>
-				</ol>
+          <ol className="breadcrumb">
+            <li><Link to="/">Home</Link></li>
+            <li className="active">Cart</li>
+          </ol>
 
-				<section className="cart-list container-fluid">
-					<div className="row">
-						{
-							cartItems.length ?
-							<div className="col-xs-12 col-md-8 col-md-offset-2">
-								<div>
-									<table className="cart-table table table-hover">
-										<thead>
-											<tr>
-												<th className="col-xs-2">Image</th>
-												<th className="col-xs-4">Name</th>
-												<th className="col-xs-2">Qty</th>
-												<th className="col-xs-2">Price</th>
-												<th className="col-xs-2">Action</th>
-											</tr>
-										</thead>
-										{
-											cartItems.map(item =>
-												<tbody key={ item.id }>
-													<CartItem item={item} removeItem={removeItem} updateQuantity={updateQuantity} />
-												</tbody>
-											)
-										}
-									</table>
-									<Link to="/checkout">
-										<button className="submit">check out</button>
-									</Link>
-								</div>
-							</div>
-							: <h2>Your cart is currently empty</h2>
-						}
-					</div>
-				</section>
-			</div>
-		);
-	}
+          <section className="cart-list container-fluid">
+            <div className="row">
+              {
+                cartItems.length ?
+                <div className="col-xs-12 col-md-8 col-md-offset-2">
+                  <div>
+                    <table className="cart-table table table-hover">
+                      <thead>
+                        <tr>
+                          <th className="col-xs-2">Image</th>
+                          <th className="col-xs-4">Name</th>
+                          <th className="col-xs-2">Qty</th>
+                          <th className="col-xs-2">Price</th>
+                          <th className="col-xs-2">Action</th>
+                        </tr>
+                      </thead>
+                      {
+                        cartItems.map(item =>
+                          <tbody key={ item.id }>
+                            <CartItem item={item} removeItem={removeItem} updateQuantity={updateQuantity} />
+                          </tbody>
+                        )
+                      }
+                    </table>
+                    <Link to="/checkout">
+                      <button className="submit">check out</button>
+                    </Link>
+                  </div>
+                </div>
+              : <h2>Your cart is currently empty</h2>
+            }
+          </div>
+        </section>
+      </div>
+    );
+  }
 }
