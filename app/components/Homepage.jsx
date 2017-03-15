5041ebc8710ee33b0188a96a8d609e9f16f95509
Aryeh Harris <harryttd@gmail.com>
2017-03-15 17:28:22 -0400

    Fixed all indent issues for components.

--- app/components/Homepage.jsx
+++ app/components/Homepage.jsx
@@ -4,28 +4,28 @@ import { Link } from 'react-router';
 import Products from './Products';
 
 const Home = ({ products }) => {
-	return (
-		<div className="home">
+  return (
+    <div className="home">
 
-			<div className="image-slide">
-				<div className="image-info">
-				</div>
-			</div>
+      <div className="image-slide">
+        <div className="image-info">
+        </div>
+      </div>
 
-			<Products products={products} />
+      <Products products={products} />
 
-			<section className="container-fluid promo-wrapper">
-				<div className="row">
-					<div className="col-xs-12 col-lg-6 our-story">
-						<Link href="/about"> <span>OUR STORY</span> </Link>
-					</div>
-					<div className="col-xs-12 col-lg-6 new-arrival">
-						<Link href="/products/category/New%20Arrivals"> <span>SHOP ARRIVALS</span>	</Link>
-					</div>
-				</div>
-			</section>
-		</div>
-	);
+      <section className="container-fluid promo-wrapper">
+        <div className="row">
+          <div className="col-xs-12 col-lg-6 our-story">
+            <Link href="/about"> <span>OUR STORY</span> </Link>
+          </div>
+          <div className="col-xs-12 col-lg-6 new-arrival">
+            <Link href="/products/category/New%20Arrivals"> <span>SHOP ARRIVALS</span>	</Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 const mapState = ({ products }) => ({ products: products.list });
--- app/components/ReviewList.jsx
+++ app/components/ReviewList.jsx
@@ -3,18 +3,18 @@ import SingleReview from './SingleReview';
 import { Link } from 'react-router';
 
 const ReviewList = ({ reviews }) => {
-	return (
-		<div>
-			{ reviews && reviews.map(review => {
-				return (
-					<Link key={review.id} to={`/reviews/${review.id}`}>
-						<SingleReview rating={reviews.rating} content={reviews.content} />
-					</Link>
-				);
-			})
-		}
-		</div>
-	);
+  return (
+    <div>
+      { reviews && reviews.map(review => {
+        return (
+          <Link key={review.id} to={`/reviews/${review.id}`}>
+            <SingleReview rating={reviews.rating} content={reviews.content} />
+          </Link>
+        );
+      })
+    }
+  </div>
+);
 };
 
 export default ReviewList;
